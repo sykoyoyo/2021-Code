2add47de88fffd73fbff2409cd7e7590d958ac63
Anders Aleckson <andersaleckson@gmail.com>
2021-01-23 12:00:45 -0600

    Update Anders.py

--- autonomous/Anders.py
+++ autonomous/Anders.py
@@ -4,31 +4,6 @@ from robotpy_ext.autonomous import StatefulAutonomous, timed_state, state
 
 class DriveForward(StatefulAutonomous):
 
-    MODE_NAME = 'Drive anders'
-
-    def initialize(self):
-
-        self.speed = -.4
-
-    @timed_state(duration=0.5, next_state='drive_forward', first=True)
-    def drive_wait(self):
-        self.myRobot.tankDrive(0,0)
-
-    @timed_state(duration=4, next_state='stop')
-    def drive_forward(self):
-        self.myRobot.tankDrive(1, 1)
-
-    @state()
-    def stop(self):
-        self.myRobot.tankDrive(0,0)
-
-
-
-
-from robotpy_ext.autonomous import StatefulAutonomous, timed_state, state
-
-class DriveForward(StatefulAutonomous):
-
     MODE_NAME = 'Drive Anders'
 
     def initialize(self):
@@ -43,12 +18,12 @@ class DriveForward(StatefulAutonomous):
     def drive_forward(self):
         self.myRobot.tankDrive(.8, .8)
 
-    @timed_state(duration=1, next_state='turn_right')
-    def drive_forward(self):
+    @timed_state(duration=2, next_state='turn_right')
+    def turn_left(self):
         self.myRobot.tankDrive(.8, 0)
 
     @timed_state(duration=1, next_state='stop')
-    def drive_forward(self):
+    def turn_right(self):
         self.myRobot.tankDrive(0, .8)
 
     @state()
