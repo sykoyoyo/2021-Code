1a782729a730c6859f8cf662ab756e33933ac791
Mr. Robby <crobertson@upsala.k12.mn.us>
2021-01-23 11:11:47 -0600

    Create forward_turn.py

---
+++ autonomous/forward_turn.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+from robotpy_ext.autonomous import StatefulAutonomous, timed_state, state
+
+class DriveForward(StatefulAutonomous):
+
+    MODE_NAME = 'Drive Forward Long'
+    DEFAULT = True
+
+    def initialize(self):
+
+        self.speed = -.4
+
+    @timed_state(duration=0.5, next_state='drive_forward', first=True)
+    def drive_wait(self):
+        self.myRobot.tankDrive(0,0)
+
+    @timed_state(duration=1, next_state='stop')
+    def drive_forward(self):
+        self.myRobot.tankDrive(.7, .7)
+
+    @timed_state(duration=1, next_state='stop')
+    def drive_forward(self):
+        self.myRobot.tankDrive(0, .7)
+
+    @state()
+    def stop(self):
+        self.myRobot.tankDrive(0,0)
