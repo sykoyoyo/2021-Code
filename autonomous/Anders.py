ccf6cff99b05b5cc6c4eca67f128a6d07591cf6c
Mr. Robby <crobertson@upsala.k12.mn.us>
2021-01-23 11:16:15 -0600

    Update Anders.py
    name change

--- autonomous/Anders.py
+++ autonomous/Anders.py
@@ -4,7 +4,7 @@ from robotpy_ext.autonomous import StatefulAutonomous, timed_state, state
 
 class DriveForward(StatefulAutonomous):
 
-    MODE_NAME = 'Drive Forward Long'
+    MODE_NAME = 'Drive anders'
     DEFAULT = True
 
     def initialize(self):
