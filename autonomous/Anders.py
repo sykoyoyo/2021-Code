54cb7df3b409cc9393ea8530764048b99d2798b2
Anders Aleckson <andersaleckson@gmail.com>
2021-01-23 12:47:32 -0600

    Update Anders.py

--- autonomous/Anders.py
+++ autonomous/Anders.py
@@ -20,26 +20,26 @@ class DriveForward(StatefulAutonomous):
 
     @timed_state(duration=3, next_state='turn_right')
     def turn_left(self):
-        self.myRobot.tankDrive(.8, 0)
+        self.myRobot.tankDrive(.8, .3)
 
-    @timed_state(duration=4, next_state='turn_left')
+    @timed_state(duration=4, next_state='turn_left2')
     def turn_right(self):
-        self.myRobot.tankDrive(0, .8)
+        self.myRobot.tankDrive(.3, .8)
 
-    @timed_state(duration=3, next_state='drive_forward')
-    def turn_left(self):
-        self.myRobot.tankDrive(.8, 0)
+    @timed_state(duration=3, next_state='drive_forward2')
+    def turn_left2(self):
+        self.myRobot.tankDrive(.8, .3)
 
-    @timed_state(duration=4, next_state='turn_right')
-    def drive_forward(self):
+    @timed_state(duration=4, next_state='turn_right2')
+    def drive_forward2(self):
         self.myRobot.tankDrive(.8, .8)
 
-    @timed_state(duration=1, next_state='drive_forward')
-    def turn_right(self):
-        self.myRobot.tankDrive(0, .8)
+    @timed_state(duration=1, next_state='drive_forward3')
+    def turn_right2(self):
+        self.myRobot.tankDrive(.3, .8)
 
     @timed_state(duration=0.5, next_state='stop')
-    def drive_forward(self):
+    def drive_forward3(self):
         self.myRobot.tankDrive(.8, .8)
 
     @state()
