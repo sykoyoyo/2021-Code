bba61d69845196251b0a7119acae11075184b542
Mr. Robby <crobertson@upsala.k12.mn.us>
2021-01-24 08:51:20 -0600

    updated comments for tutorials

--- autonomous/drive_forward.py
+++ autonomous/drive_forward.py
@@ -3,22 +3,43 @@
 from robotpy_ext.autonomous import StatefulAutonomous, timed_state, state
 
 class DriveForward(StatefulAutonomous):
+    #Required first two lines of Text!
 
-    MODE_NAME = 'Drive Forward Long'
-    DEFAULT = True
+
+    MODE_NAME = 'Drive Forward Long' - #This is the name the Auto is called
+    DEFAULT = True  #only ONE Auto can have this- Makes it the default if none are chosen.  If you have more than one Auto with it on there, it will not work
 
     def initialize(self):
+        #you could add timers here or additional sensors etc
+        pass
 
-        self.speed = -.4
 
     @timed_state(duration=0.5, next_state='drive_forward', first=True)
+    #@timed_state is required( Duration is length in seconds of first move,
+    #next_state - says which line to go to AFTER completing this one,
+    #first=True - Start here for automodes)
+
     def drive_wait(self):
+        #def required - but then you use a Name for this move.
+        #In this instance pick something that would match it's objective for
+        #this one it is waiting, so "Drive wait"
+
         self.myRobot.tankDrive(0,0)
+        #What does the robot do, Now you call your variables from robot.py
+        #that were setup for Auto, and call them here.
+        #Tank Drive (left, right)
+
 
     @timed_state(duration=4, next_state='stop')
     def drive_forward(self):
         self.myRobot.tankDrive(-1,-1)
 
+        #This is a complete autonomous command, Note the Duration 4 seconds,
+        #Next command is stop, Naming is Drive forward, and tank drive 1,1
+
+
     @state()
     def stop(self):
         self.myRobot.tankDrive(0,0)
+
+        #This is the last statement given, and is a Stop command.
--- robot.py
+++ robot.py
@@ -6,61 +6,101 @@ import ctre
 import wpilib.drive
 from robotpy_ext.autonomous import AutonomousModeSelector
 
+"""
+Import is pulling all the libraries and API's you need for your code,
+The most common ones are posted above.
+"""
 
 
 class MyRobot(wpilib.TimedRobot):
-    """
-    This is a short sample program demonstrating how to use the basic throttle
-    mode of the TalonSRX
-    """
+"""
+This is a short sample program demonstrating how to call various components
+and to setup basic AutonomousModeSelector
+"""
 
     def robotInit(self):
 
+"""
+setup Motors and Speed Controls first
+"""
 
         self.motor1 = ctre.WPI_TalonSRX(1)  # Initialize the TalonSRX on device 1.
         self.motor2 = ctre.WPI_TalonSRX(2)
         self.motor3 = ctre.WPI_TalonSRX(3)
         self.motor4 = ctre.WPI_TalonSRX(4)
-        #self.motor5 = ctre.TalonFX(5)
-        self.joy = wpilib.Joystick(0)
+        #self.motor5 = ctre.TalonFX(5)  #Motor is programmed, but not plugged in yet
+        self.joy = wpilib.XboxController(0) #this is a controller, also acceptable to use Joystick
 
-        #self.arm = wpilib.Solenoid(1)
+        #self.arm = wpilib.Solenoid(1) #calling a solenoid to be used with Pneumatics
 
         self.left = wpilib.SpeedControllerGroup(self.motor1, self.motor2)
         self.right = wpilib.SpeedControllerGroup(self.motor3, self.motor4)
+"""
+This is combining the Speed controls from above to make a left and right
+for the drive chassis. Note that self.motor1 and self.motor2 are combined to make self.left
+then self.motor3 and self.motor4 are combined to make self.right. This is done to Make
+the differential drive easier to setup
+"""
 
         self.myRobot = wpilib.drive.DifferentialDrive(self.left, self.right)
+#Here is our DifferentialDrive, Ultimately stating, Left side and Right side of chassis.
+
         self.myRobot.setExpiration(0.1)
 
+"""
+These components here are for Autonomous Modes, and allows you to call parts and
+components here to be used IN automodes. For example- Our chassis above is labeled
+'self.myRobot', below in the self.components, If we want to use our chassis to drive
+in Autonomous, we need to call it in the fashion below.  It is also encouraged to
+reuse the naming of the components from above to avoid confusion below. i.e.
+'Chassis': self.myRobot, In autonomous creation, I would then be using the variable
+self.chassis.set() instead of self.myRobot.set() when doing commands.
+"""
 
         self.components = {
             'myRobot': self.myRobot,
-            #'motor5': self.motor5,
-            #'arm': self.arm
+            #'motor5': self.motor5, #again a speed control not setup yet
+            #'arm': self.arm #pneumatics arm that is not setup on bot yet
         }
 
         self.automodes = AutonomousModeSelector('autonomous', self.components)
-
+"""
+This line is to label where our automodes folder is and what devices used,
+('Insert folder name here', What compenents used in your auto codes (See above)
+"""
 
     def autonomousInit(self):
-        """This function is run once each time the robot enters autonomous mode."""
+"""
+This function is run once each time the robot enters autonomous mode.
+"""
         pass
 
     def autonomousPeriodic(self):
-        """This function is called periodically during autonomous."""
+"""
+This function is called periodically during autonomous.
+"""
         self.automodes.run()
 
 
     def teleopInit(self):
-        """Executed at the start of teleop mode"""
+"""
+Executed at the start of teleop mode
+"""
         self.myRobot.setSafetyEnabled(True)
 
 
     def teleopPeriodic(self):
-        """Runs Robot on Tank Drive like a skid steer, two joysticks"""
+"""
+Runs Robot on Arcade Drive and pulls
+"""
 
-        self.myRobot.arcadeDrive(-1*self.joy.getRawAxis(1), self.joy.getRawAxis(0), .7)
-        """
+        self.myRobot.arcadeDrive(-1*self.joy.getLeftX(), self.joy.getLeftY())
+
+"""
+Below is an example code to be used for when a button is pressed
+do something
+"""
+
         if self.joy.getAButton():
             self.arm.set(True)
             self.motor5.set(.25)
@@ -70,7 +110,12 @@ class MyRobot(wpilib.TimedRobot):
                 self.motor5.set(-1)
             else:
                 self.motor5.set(0)
-                    """
+"""
+If the A button on the XboxController is pushed, Set our Solenoid Arm forward
+and turn on the intake motor to 25% power. else- Set the Pneumatic arm to rest
+or back at home, then If the X button is pushed, Rev up our Intake motor to
+be a shooter at full speed. Else (Otherwise) if nothing is pushed, go to rest
+"""
 
 if __name__ == "__main__":
     wpilib.run(MyRobot)
