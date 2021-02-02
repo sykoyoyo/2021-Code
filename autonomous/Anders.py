213361ac5a888044c185f2e4e23acd74dae746ad
Anders Aleckson <andersaleckson@gmail.com>
2021-02-02 11:33:17 -0600

    Update Anders.py

--- autonomous/Anders.py
+++ autonomous/Anders.py
@@ -14,32 +14,8 @@ class DriveForward(StatefulAutonomous):
     def drive_wait(self):
         self.myRobot.tankDrive(0, 0)
 
-    @timed_state(duration=2, next_state='turn_left')
+    @timed_state(duration=2, next_state='turn_right')
     def drive_forward(self):
-        self.myRobot.tankDrive(.8, .8)
-
-    @timed_state(duration=3.45, next_state='drive_forward2')
-    def turn_left(self):
-        self.myRobot.tankDrive(.6, .85)
-
-    @timed_state(duration=1.15, next_state='turn_left2')
-    def drive_forward2(self):
-        self.myRobot.tankDrive(.8, .8)
-
-    @timed_state(duration=1.4, next_state='drive_forward3')
-    def turn_left2(self):
-        self.myRobot.tankDrive(.85, .6)
-
-    @timed_state(duration=1, next_state='turn_left3')
-    def drive_forward3(self):
-        self.myRobot.tankDrive(.8, .8)
-
-    @timed_state(duration=1, next_state='drive_forward4')
-    def turn_left3(self):
-        self.myRobot.tankDrive(.6, .85)
-
-    @timed_state(duration=2.5, next_state='stop')
-    def drive_forward4(self):
         self.myRobot.tankDrive(.8, .8)
 
     @state()
