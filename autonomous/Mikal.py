7ccee24a4c408285e7ab9add23b3690e53aeb3f6
Mr. Robby <crobertson@Upsala.k12.mn.us>
2021-02-23 11:16:54 -0600

    Slalom Prevision

--- autonomous/Mikal.py
+++ autonomous/Mikal.py
@@ -14,9 +14,53 @@ class DriveForward(StatefulAutonomous):
     def drive_wait(self):
         self.myRobot.tankDrive(0,0)
 
-    @timed_state(duration=4, next_state='stop')
+    @timed_state(duration=0.5, next_state='turn_left')
     def drive_forward(self):
-        self.myRobot.tankDrive(1, 1)
+        self.myRobot.tankDrive(.8, .8)
+
+    @timed_state(duration=0.5, next_state='drive_forward2')
+    def turn_left(self):
+        self.myRobot.tankDrive(.2, .8)
+
+    @timed_state(duration=0.5, next_state='turn_right')
+    def drive_forward2(self):
+        self.myRobot.tankDrive(.8, .8)
+
+    @timed_state(duration=0.5, next_state='drive_forward3')
+    def turn_right(self):
+        self.myRobot.tankDrive(.8, .2)
+
+    @timed_state(duration=3, next_state='turn_right2')
+    def drive_forward3(self):
+        self.myRobot.tankDrive(.8, .8)
+
+    @timed_state(duration=1, next_state='turn_left2')
+    def turn_right2(self):
+        self.myRobot.tankDrive(.8, .35)
+
+    @timed_state(duration=4, next_state='drive_forward4')
+    def turn_left2(self):
+        self.myRobot.tankDrive(.7, .8)
+
+    @timed_state(duration=0.5, next_state='turn_right3')
+    def drive_forward4(self):
+        self.myRobot.tankDrive(.8, .8)
+
+    @timed_state(duration=0.5, next_state='drive_forward5')
+    def turn_right3(self):
+        self.myRobot.tankDrive(.2, .8)
+
+    @timed_state(durration=3, next_state='turn_right4')
+    def drive_forward5(self):
+        self.myRobot.tankDrive(.8, .8)
+
+    @timed_state(duration=1, next_state='turn_left4')
+    def turn_right4(self):
+        self.myRobot.tankDrive(.8, .35)
+
+    @timed_state(duration=0.5, next_state='stop')
+    def turn_left4(self):
+        self.myRobot.tankDrive(.5, .8)
 
     @state()
     def stop(self):
