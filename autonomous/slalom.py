8fab8309aafcbc569acb86c555dc8395bbd4804d
Mr. Robby <crobertson@Upsala.k12.mn.us>
2021-03-11 11:19:43 -0600

    Update slalom.py

--- autonomous/slalom.py
+++ autonomous/slalom.py
@@ -13,7 +13,7 @@ class DriveForward(StatefulAutonomous):
     def drive_wait(self):
         self.myRobot.tankDrive(0,0)
 
-    @timed_state(duration=0.5, next_state='turn_left')
+    @timed_state(duration=0.6, next_state='turn_left')
     def drive_forward(self):
         self.myRobot.tankDrive(.8, .8)
 
@@ -21,13 +21,25 @@ class DriveForward(StatefulAutonomous):
     def turn_left(self):
         self.myRobot.tankDrive(.5, .8)
 
-    @timed_state(duration=0.6, next_state='drive_forward2')
+    @timed_state(duration=1, next_state='drive_forward2')
     def turn_right(self):
         self.myRobot.tankDrive(.8, .5)
 
-    @timed_state(duration=2, next_state='turn_right2')
+    @timed_state(duration=1.3, next_state='turn_right2')
     def drive_forward2(self):
         self.myRobot.tankDrive(.8, .8)
+
+    @timed_state(duration=0.9, next_state='drive_forward3')
+    def turn_right2(self):
+        self.myRobot.tankDrive(.7, .38)
+
+    @timed_state(duration=0, next_state='turn_left2')
+    def drive_forward3(self):
+        self.myRobot.tankDrive(.8, .8)
+
+    @timed_state(duration=1.7, next_state='turn_right3')
+    def turn_left2(self):
+        self.myRobot.tankDrive(.5, .8)
 
     @state()
     def stop(self):
